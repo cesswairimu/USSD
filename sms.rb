8b436aaf26f453863c3050208673bb9cea216175
Cess <cessmbuguar@gmail.com>
2016-10-29 11:05:30 +0300

    sending sms

---
+++ sms.rb
@@ -0,0 +1,4 @@
+require 'AfricasTalkingGateway'
+apikey = '7c6b8114bd0bdf7375e8dae96964e754ddc8aff41be90e4745dd32113e6e3aaa'
+gateway = AfricasTalkingGateway.new('cesswairimu254', apikey)
+gateway.sendMessage( +254700324708,"Hello  we made it!!")
